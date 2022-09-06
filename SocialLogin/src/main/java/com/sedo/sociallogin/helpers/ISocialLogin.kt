bf61b7e3fa1897acc5453acd935abf5752ed43ca
saed.abusham@techzone.com <0799845399Ss>
2022-09-06 14:36:08 +0300

    under dev

--- SocialLogin/src/main/java/com/sedo/sociallogin/helpers/ISocialLogin.kt
+++ SocialLogin/src/main/java/com/sedo/sociallogin/helpers/ISocialLogin.kt
@@ -12,15 +12,15 @@ import com.sedo.sociallogin.`interface`.SocialLoginCallBack
 abstract class ISocialLogin(
     val activity: ComponentActivity?,
     val fragment: Fragment?,
-    val clientId:String?=null
+    val clientId: String? = null
 ) {
     abstract fun showError(show: Boolean)
     abstract fun setCallBack(socialLoginCallBack: SocialLoginCallBack)
     abstract fun setRegisterResult(resultLauncher: ActivityResultLauncher<Intent>)
     abstract fun initMethod()
     abstract fun startMethod()
-    open fun setResult(data: Any){}
-    open fun handleResult(data: Any){}
+    open fun setResult(data: Any) {}
+    open fun handleResult(data: Any) {}
 
     fun getContext() = activity ?: fragment?.requireContext()
     fun getWindow() = activity?.window ?: fragment?.requireActivity()?.window
@@ -29,13 +29,13 @@ abstract class ISocialLogin(
     protected var socialLoginCallBack: SocialLoginCallBack? = null
     protected var resultLauncher: ActivityResultLauncher<Intent>? = null
     protected var defaultResultLauncher: ActivityResultLauncher<Intent>? =
-        activity?.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        fragment?.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
                 val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
                 handleResult(task)
             }
         }
-            ?: fragment?.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            ?: activity?.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
                 if (result.resultCode == Activity.RESULT_OK) {
                     val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
                     handleResult(task)
